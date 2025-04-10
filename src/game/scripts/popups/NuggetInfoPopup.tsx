37bf0c26ca72810d66c9033897a5f2fd142a5cf7
charlie275691502 <charlie275691502@gmail.com>
2025-04-10 21:17:31 +1000

    nugget info popup

--- src/game/scripts/popups/NuggetInfoPopup.tsx
+++ src/game/scripts/popups/NuggetInfoPopup.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import background from "../../images/popups/pop_frame.png";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
 import "./NuggetInfoPopup.css";
 import { selectUIState, setUIState, UIStateType } from "../../../data/ui";
 import { NuggetData } from "../../../data/model";
+import { getTextShadowStyle } from "../common/Utility";
+import NuggetLevel from "../scene/gameplay/NuggetLevel";
+import image from "../../images/nuggets/image.png";
+import DefaultButton from "../buttons/DefaultButton";
 
 interface Props {
   nuggetData: NuggetData;
@@ -11,7 +15,31 @@ interface Props {
 
 const NuggetInfoPopup = ({ nuggetData }: Props) => {
   const dispatch = useAppDispatch();
+  const containerRef = useRef<HTMLParagraphElement>(null);
   const uIState = useAppSelector(selectUIState);
+  const [titleFontSize, setTitleFontSize] = useState<number>(0);
+  const [descriptionFontSize, setDescriptionFontSize] = useState<number>(0);
+  const nuggetId = nuggetData.id;
+  const nuggetPrice = nuggetData.sysprice;
+  const nuggetCycle = nuggetData.cycle;
+  const nuggetLevel = nuggetData.feature;
+  const nuggetBid = nuggetData.bid?.bidprice ?? 0;
+
+  const adjustSize = () => {
+    if (containerRef.current) {
+      setTitleFontSize(containerRef.current.offsetHeight / 10);
+      setDescriptionFontSize(containerRef.current.offsetHeight / 13);
+    }
+  };
+
+  useEffect(() => {
+    adjustSize();
+
+    window.addEventListener("resize", adjustSize);
+    return () => {
+      window.removeEventListener("resize", adjustSize);
+    };
+  }, [containerRef.current]);
 
   const onClickCancel = () => {
     if (uIState.type == UIStateType.NuggetInfoPopup) {
@@ -19,11 +47,77 @@ const NuggetInfoPopup = ({ nuggetData }: Props) => {
     }
   };
 
+  const onClickExploreNugget = () => {
+    // dispatch(setUIState({ type: UIStateType.Idle }));
+  };
+
+  const onClickSellNugget = () => {
+    // dispatch(setUIState({ type: UIStateType.Idle }));
+  };
+
   return (
     <div className="nugget-info-popup-container">
       <div onClick={onClickCancel} className="nugget-info-popup-mask" />
-      <div className="nugget-info-popup-main-container">
+      <div ref={containerRef} className="nugget-info-popup-main-container">
+        <img src={image} className="nugget-info-popup-avatar-image" />
         <img src={background} className="nugget-info-popup-main-background" />
+        <p
+          className="nugget-info-popup-title-text"
+          style={{
+            fontSize: titleFontSize,
+            ...getTextShadowStyle(titleFontSize / 15),
+          }}
+        >
+          {`NuggetId: ${nuggetId}`}
+        </p>
+        <p
+          className="nugget-info-popup-price-text"
+          style={{
+            fontSize: descriptionFontSize,
+            ...getTextShadowStyle(descriptionFontSize / 15),
+          }}
+        >
+          {`Recycle Price: ${nuggetPrice}`}
+        </p>
+        <p
+          className="nugget-info-popup-cycle-text"
+          style={{
+            fontSize: descriptionFontSize,
+            ...getTextShadowStyle(descriptionFontSize / 15),
+          }}
+        >
+          {`Cycle: ${nuggetCycle}`}
+        </p>
+        <p
+          className="nugget-info-popup-bid-text"
+          style={{
+            fontSize: descriptionFontSize,
+            ...getTextShadowStyle(descriptionFontSize / 15),
+          }}
+        >
+          {`Bid: ${nuggetBid}`}
+        </p>
+        <div className="nugget-info-popup-levels-container">
+          {Array.from({ length: 7 }).map((_, index) => (
+            <div key={index} className={`nugget-info-popup-level-container`}>
+              <NuggetLevel key={index} isActive={index < nuggetLevel} />
+            </div>
+          ))}
+        </div>
+        <div className="nugget-info-popup-explore-button">
+          <DefaultButton
+            text={"Explore Nugget"}
+            onClick={onClickExploreNugget}
+            isDisabled={false}
+          />
+        </div>
+        <div className="nugget-info-popup-sell-button">
+          <DefaultButton
+            text={"Sell Nugget"}
+            onClick={onClickSellNugget}
+            isDisabled={false}
+          />
+        </div>
       </div>
     </div>
   );
